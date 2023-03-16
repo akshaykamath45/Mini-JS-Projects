08189dad1e1461591f11292082062c657ccbccbb
Akshay Kamath <akshay.kamath054@nmims.edu.in>
2023-03-16 16:37:44 +0530

    Creating Toast Notification
    •toast container floating
    •toast design
    •button
    •click show random toast message

---
+++ Toast Notifications/script.js
@@ -0,0 +1,15 @@
+const btn=document.getElementById('btn');
+const container=document.getElementById('container');
+btn.addEventListener('click',()=>{
+    createNotification();
+});
+
+function createNotification(){
+    const notif=document.createElement('div');
+    notif.classList.add('toast');;
+    container.appendChild(notif);
+    notif.innerText="This is a notification";
+    setTimeout(()=>{
+        notif.remove();
+    },3000);
+}
